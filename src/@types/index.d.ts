b976f7c87b9a6192c8c03526980d37db4e628e5a
AlexTaietti <taiettialex@yahoo.it>
2021-04-09 18:33:18 +0100

    🚀 IMPROVEMENT: update types and declarations

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -7,6 +7,30 @@ export type PerlinTextProps = {
    };
 };
 
+export type ButtonsContainerProps = {
+   demoLink: string;
+   codeLink?: string;
+   SpecialButton?: React.FC<{ handleClick?: () => void }>;
+   specialButtonText?: string;
+   specialButtonClickHandler?: () => void;
+};
+
+export type DescriptionProps = {
+   demoLink: string;
+   codeLink?: string;
+   info: {
+      text: string;
+      color?: string;
+   };
+   title: {
+      text: string;
+      color?: string;
+   };
+   SpecialButton?: React.FC<{ handleClick?: () => void }>;
+   specialButtonText?: string;
+   specialButtonClickHandler?: () => void;
+};
+
 export type Vec2D = { x: number, y: number };
 
 export type Square = { width: number, height: number };
