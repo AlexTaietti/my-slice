ec8f3083966980d4260f774bb88190bc07ae070a
AlexTaietti <taiettialex@yahoo.it>
2021-04-10 17:57:42 +0100

    🚀 IMPROVEMENT: add a cute little hover animation to the demo button

--- src/components/DemoLink.tsx
+++ src/components/DemoLink.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const DemoLink: React.FC<{ link: string }> = ({ link }) => {
 
@@ -10,18 +10,37 @@ export const DemoLink: React.FC<{ link: string }> = ({ link }) => {
 
 };
 
+const takeoff = keyframes`
+
+   0%{ transform: translate(0px, 0px); }
+   49.999%{ transform: translate(40px, -40px); }
+   50%{ transform: translate(-40px, 40px); }
+   100%{ transform: translate(0px, 0px); }
+
+`;
+
 const DemoButton = styled.button`
 
    background: #414344;
    color: white;
    transition-property: background;
    transition-duration: .4s;
+   overflow: hidden;
 
    i{
       line-height: 15px;
       margin-right: 10px;
+      animation-duration: 1.2s;
+      animation-timing-function: cubic-bezier(.4,-0.63,.53,1.59);
+      animation-fill-mode: backwards;
    }
 
-   &:hover{ background: #9c07c6; }
+   &:hover{
+      
+      background: #9c07c6;
+
+      i{ animation-name: ${takeoff}; }
+   
+   }
 
 `
